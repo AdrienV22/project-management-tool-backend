610b2aaf0d1196e4069b3e3775e2efacde06fa40
Adrien Valo <adrien.valo@outlook.fr>
2024-12-14 15:04:05 +0100

    user table added

--- src/main/java/com/example/project_management_tool/entity/User.java
+++ src/main/java/com/example/project_management_tool/entity/User.java
@@ -1,17 +1,14 @@
 package com.example.project_management_tool.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // identifiant unique pour chaque utilisateur
-
     private String username;
     private String email;
     private String password;
@@ -52,4 +49,3 @@ public class User {
         this.password = password;
     }
 }
-
