fa3b41244166f32c23a63b9f255fd63c4742b9f4
Adrien Valo <adrien.valo@outlook.fr>
2024-12-14 23:01:27 +0100

    ajout de l'endpoint our récuperer le projet en fonction de l'ID

--- src/main/java/com/example/project_management_tool/controller/ProjectController.java
+++ src/main/java/com/example/project_management_tool/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.example.project_management_tool.controller;
 
 import com.example.project_management_tool.model.ProjectModel;
+import com.example.project_management_tool.model.TaskModel;
 import com.example.project_management_tool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,17 @@ public class ProjectController {
     public ProjectModel createProject(@RequestBody ProjectModel project) {
         return projectRepository.save(project);
     }
+
+    // Endpoint pour récupérer le projet en fonction de l'ID
+    public ProjectModel getProjectById(Long id) {
+        return projectRepository.findById(id).orElse(null);
+    }
+
+    public ProjectModel addTaskById(TaskModel task, ProjectModel project) {
+        project.getTaskList().add(task);
+        return projectRepository.save(project);
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
