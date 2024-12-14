77b8e4f788b4bae15db683cfc17792794a55b3ae
Adrien Valo <adrien.valo@outlook.fr>
2024-12-14 16:07:00 +0100

    modification du Task.java pour gérer les caractères Ãmax

--- src/main/java/com/example/project_management_tool/model/Task.java
+++ src/main/java/com/example/project_management_tool/model/Task.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.time.LocalDate;
 
@@ -13,8 +16,17 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min = 5, max = 50, message = "Project Title can't exceed size limit (5-50 characters)")
     private String title;
+
+    @Size(min = 5, max =250, message = "Project Title can't exceed size limit (5-50 characters)")
     private String description;
+
     private LocalDate dueDate;
+
+    @NotNull
+    @Pattern(regexp = "En cours|Terminé|En attente", message = "Status must be one of: En cours, Terminé, En attente")
     private String status; // Exemple : "En cours", "Terminé", "En attente"
 }
