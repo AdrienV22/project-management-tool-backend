e507a0d88d604ed8fcfe8b8b2421ecd162bd3513
Adrien Valo <adrien.valo@outlook.fr>
2024-12-14 16:00:15 +0100

    correction d'erreur dans modelProject.java

--- src/main/java/com/example/project_management_tool/model/Project.java
+++ src/main/java/com/example/project_management_tool/model/Project.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.time.LocalDate;
 
@@ -13,8 +15,14 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Project name cannot be null")
+    @Size(min = 1, max = 100, message = "Project name must respect size limit (1-100 characters)")
     private String name;
+
+    @Size(max = 500, message = "Description must respect size limit (500 characters)")
     private String description;
+
+    @NotNull(message = "Start date cannot be null")
     private LocalDate startDate;
 }
-
