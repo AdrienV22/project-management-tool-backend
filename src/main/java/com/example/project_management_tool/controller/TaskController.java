f13811ad1bf2da2df9b8cb761954cad15aac2508
Adrien Valo <adrien.valo@outlook.fr>
2024-12-14 15:45:18 +0100

    modification du fichier TaskController.java avec ajout du @valid dans le @PostMapping

--- src/main/java/com/example/project_management_tool/controller/TaskController.java
+++ src/main/java/com/example/project_management_tool/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.project_management_tool.controller;
 
 import com.example.project_management_tool.model.Task;
 import com.example.project_management_tool.repository.TaskRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,15 +21,15 @@ public class TaskController {
         return taskRepository.findAll();
     }
 
-    // Endpoint pour créer une tâche
-    @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task);
-    }
-
     // Endpoint pour récupérer une tâche par son ID
     @GetMapping("/{id}")
     public Task getTaskById(@PathVariable Long id) {
         return taskRepository.findById(id).orElse(null);
+    }
+
+    // Endpoint pour créer une tâche
+    @PostMapping
+    public Task createTask(@Valid @RequestBody Task task) {
+        return taskRepository.save(task);
     }
 }
